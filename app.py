0df83c6bbc9f27e4060a09788f19a3bf8a2f7aa0
SamuelVn007 <134400857+SamuelVn007@users.noreply.github.com>
2024-08-23 07:42:39 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+print ("""
+█░░ █▀█ ░░█ ▄▀█ █▀   █▀█ █░█ ▄▀█ █▀█ ▄▀█
+█▄▄ █▄█ █▄█ █▀█ ▄█   █▄█ █▀█ █▀█ █▀▄ █▀█""")
+
+print('1. cadastra site')
+print('2. listar site')
+print('3. ativar site')
+print('4. sair')
+
+opcao_escolhida = int(input('escolha uma opção'))
+
+if opcao_escolhida == 1:
+    print('Você escolheu a opção cadastrar site')
+elif opcao_escolhida== 2:
+    print('Você escolheu a opção listar site ')   
+elif opcao_escolhida== 3:
+    print('Você escolheu a opção ativar site ')  
+elif opcao_escolhida== 4:
+    print('Você escolheu a opção sair ')  
